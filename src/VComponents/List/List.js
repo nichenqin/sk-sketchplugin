45eaeea151345fa1518b1c3e713cf0f51786182f
nichenqin <nichenqin@gmail.com>
2017-12-30 21:13:50 +0800

    add title

--- src/VComponents/List/List.js
+++ src/VComponents/List/List.js
@@ -17,30 +17,50 @@ class List extends VComponent {
   }
 
   importToSketch({ rows, columns }) {
-    const { page, name } = this;
+    const { sketch, page, name } = this;
 
     const group = page.newGroup({ name });
-    const row = group.newGroup({ name: 'row' });
 
-    const itemInstance = this.createSymbolInstanceByPath('list/body/single');
-    const rect = getRectOfNativeLayer(itemInstance);
+    // region title
+    const title = group.newGroup({ name: 'title' });
+    const titleInstance = this.createSymbolInstanceByPath('list/header/normal');
+    const rect = getRectOfNativeLayer(titleInstance);
     this.setState(rect);
+    const { width, height } = this.state;
 
+    const titleInstances = [...new Array(columns)].map(() => titleInstance.copy());
+    title.sketchObject.addLayers(titleInstances);
+
+    let titleIndex = 0;
+    title.iterate(layer => {
+      layer.sketchObject.frame().setX_(width * titleIndex);
+      titleIndex += 1;
+    });
+    title.adjustToFit();
+    // endregion title
+
+    // region rows
+    const row = group.newGroup({ name: 'row' });
+    const itemInstance = this.createSymbolInstanceByPath('list/body/single');
     const instances = [...new Array(columns)].map(() => itemInstance.copy());
     row.sketchObject.addLayers(instances);
+    setFrame(row, { y: height });
 
-    let index = 0;
+    let itemIndex = 0;
     row.iterate(layer => {
-      layer.sketchObject.frame().setX_(this.state.width * index);
-      index += 1;
+      layer.sketchObject.frame().setX_(width * itemIndex);
+      itemIndex += 1;
     });
     row.adjustToFit();
+    // endrigion rows
 
+    // region duplicate rows
     for (let i = 1; i < rows; i += 1) {
       const newRow = row.duplicate();
       newRow.adjustToFit();
-      setFrame(newRow, { y: this.state.height * i });
+      setFrame(newRow, { y: height * (i + 1) });
     }
+    // endregion duplicate rows
 
     group.adjustToFit();
     group.select();
