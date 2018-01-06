3f3b67e6cf7ae732e3b024e30adea1dec69f7211
nichenqin <nichenqin@gmail.com>
2018-01-06 16:22:40 +0800

    add header to datepicker

--- src/SketchComponents/Datepicker/Datepicker.js
+++ src/SketchComponents/Datepicker/Datepicker.js
@@ -1,5 +1,5 @@
 import SketchComponent from '../SketchComponent';
-import { getRectOfNativeLayer } from '../../utils';
+import { getRectOfNativeLayer, isOverridePointName } from '../../utils';
 
 const option = {
   name: 'datepicker',
@@ -16,11 +16,35 @@ class Datepicker extends SketchComponent {
     const { page, name } = this;
     const datepickerGroup = page.newGroup({ name });
 
+    const headerInstance = this.createSymbolInstanceByPath('datepicker/header/normal');
     const lightInstance = this.createSymbolInstanceByPath('datepicker/day/light');
     const selectedInstance = this.createSymbolInstanceByPath('datepicker/day/selected');
     const normalInstance = this.createSymbolInstanceByPath('datepicker/day/normal');
     const weekInstance = this.createSymbolInstanceByPath('datepicker/week');
     const { height, width } = getRectOfNativeLayer(normalInstance);
+
+    datepickerGroup.sketchObject.addLayer(headerInstance);
+    headerInstance.overridePoints().forEach(overridePoint => {
+      if (isOverridePointName(overridePoint, 'date')) {
+        const date = new Date();
+        headerInstance.setValue_forOverridePoint(String(date.getFullYear()), overridePoint);
+      }
+    });
+    datepickerGroup.adjustToFit();
+
+    const weekData = ['日', '一', '二', '三', '四', '五', '六'];
+    const weekInstances = weekData.map(() => weekInstance.copy());
+    datepickerGroup.sketchObject.addLayers(weekInstances);
+
+    weekInstances.forEach((week, index) => {
+      week.frame().setX_(width * index);
+      week.frame().setY_(datepickerGroup.frame.height);
+      week.overridePoints().forEach(overridePoint => {
+        week.setValue_forOverridePoint(String(weekData[index]), overridePoint);
+      });
+    });
+
+    datepickerGroup.adjustToFit();
 
     const previousInstances = previousMonthDateList.map(() => lightInstance.copy());
     const currentInstances = currentMonthDateList.map(({ day }) => (day === currentDay ? selectedInstance.copy() : normalInstance.copy()));
@@ -33,7 +57,7 @@ class Datepicker extends SketchComponent {
       const row = Math.floor(index / 7);
       const column = index % 7;
       day.frame().setX_(width * column);
-      day.frame().setY_(height * row);
+      day.frame().setY_(height * row + datepickerGroup.frame.height);
       day.overridePoints().forEach(overridePoint => {
         day.setValue_forOverridePoint(String(dateList[index].day), overridePoint);
       });
@@ -41,21 +65,7 @@ class Datepicker extends SketchComponent {
 
     datepickerGroup.adjustToFit();
 
-    const weekData = ['日', '一', '二', '三', '四', '五', '六'];
-    const weekInstances = weekData.map(() => weekInstance.copy());
-    datepickerGroup.sketchObject.addLayers(weekInstances);
-
-    weekInstances.forEach((week, index) => {
-      week.frame().setX_(width * index);
-      week.frame().setY_(height * -1);
-      week.overridePoints().forEach(overridePoint => {
-        week.setValue_forOverridePoint(String(weekData[index]), overridePoint);
-      });
-    });
-
-    datepickerGroup.adjustToFit();
     this.createBgAtGroup(datepickerGroup);
-    this.document.centerOnLayer(datepickerGroup);
     datepickerGroup.iterate(layer => {
       layer.sketchObject.setIsLocked(true);
     });
