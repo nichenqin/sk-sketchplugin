f9ecde7ac86efed0c528dd6acaa9003f62f05282
nichenqin <nichenqin@gmail.com>
2018-01-09 10:34:57 +0800

    adjust timepicker rect

--- src/SketchComponents/Timepicker/Timepicker.js
+++ src/SketchComponents/Timepicker/Timepicker.js
@@ -1,5 +1,6 @@
 import SketchComponent from '../SketchComponent';
 import Picker from '../Picker';
+import { getRectOfNativeLayer, setFrame } from '../../utils/index';
 
 const option = {
   name: 'timepicker',
@@ -15,19 +16,26 @@ class Timepicker extends SketchComponent {
 
     const rootGroup = page.newGroup({ name });
 
+    let pickerWidth = 0;
     if (showPicker) {
       const picker = new Picker(context);
       const pickerInstance = picker.layer.sketchObject;
       rootGroup.sketchObject.addLayer(pickerInstance.copy());
       page.sketchObject.removeLayer(pickerInstance);
       rootGroup.adjustToFit();
+      pickerWidth = getRectOfNativeLayer(pickerInstance).width;
     }
 
     const timepickerInstance = this.createSymbolInstanceByPath('timepicker/body');
     const timepickerGroup = rootGroup.newGroup({ name });
     timepickerGroup.sketchObject.addLayer(timepickerInstance);
 
+    const { width: timepickerWidth } = getRectOfNativeLayer(timepickerInstance);
+    const padding = pickerWidth ? (pickerWidth - timepickerWidth) / 2 : 0;
+    timepickerInstance.frame().setX_(padding);
+
     timepickerGroup.adjustToFit();
+    setFrame(timepickerGroup, { y: rootGroup.frame.height });
     this.createBgAtGroup(timepickerGroup);
     this.createShadowAtGroup(timepickerGroup);
 
